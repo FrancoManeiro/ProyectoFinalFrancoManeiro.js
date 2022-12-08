01dede1458ecf8f92d0574273e4d4b91a3aee6ba
FrancoManeiro <francomaneiro15@gmail.com>
2022-12-08 16:51:19 -0300

    dejé el promise como estaba, con el ../js/app.js

--- js/app.js
+++ js/app.js
@@ -233,7 +233,7 @@ buttonGrande.addEventListener ("click", agregarServicioCarrito);
 
 // ejecuciones
 // ---------
-fetch('https://francomaneiro.github.io/ProyectoFinalFrancoManeiro.js/')
+fetch('../json/data.json')
     .then((response) => response.json())
     .then((data) => {
         allServicios = data;
